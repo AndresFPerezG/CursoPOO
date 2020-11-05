ccc1cb68a5e5373ac4c161351b18ac2bd95dd7b8
AndresFPerezG <andresfe52@gmail.com>
2020-11-04 19:38:55 -0500

    V1 empezando con python y JAVA POO

---
+++ Java/Route.java
@@ -0,0 +1,9 @@
+package Java;
+
+import java.util.ArrayList;
+
+class Route {
+    Integer id;
+    ArrayList<Double> start; //Acá van las coordenadas de inicio
+    ArrayList<Double> end;  //Acá van las coordenadas de llegada -> Double porque las coordenadas es un decimal muy grande
+}
