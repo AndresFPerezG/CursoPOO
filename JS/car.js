b8a666da7a144796dc47753ddc4ecaede8aea7ba
AndresFPerezG <andresfe52@gmail.com>
2020-11-04 20:10:05 -0500

    creando las clases en JS y PHP

---
+++ JS/car.js
@@ -0,0 +1,8 @@
+class Car{
+    constructor(id, license, driver, passanger){
+        this.id = id
+        this.license = license
+        this.driver = driver
+        this.passanger = passanger
+    }
+}
